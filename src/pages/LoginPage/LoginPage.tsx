03eacfac8270d00ce5a151a2699de53d8dbbefac
Kirill Medvedev <repkomail@gmail.com>
2025-05-03 01:15:14 +0200

    feat: implement JSX and styles for login-page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,44 @@
 import "./LoginPage.scss";
 
 export default function LoginPage() {
-  return <h1>Login Page</h1>;
+  return (
+    <>
+      <div className="login-wrapper">
+        <div className="login-image"></div>
+        <div className="login-container">
+          <div className="form-container">
+            <h2 className="form-title">Sign In</h2>
+            <form className="form-submit" action="submit">
+              <div className="input-name-container">
+                <label className="input-name-title">EMAIL</label>
+                <input
+                  className="input-name"
+                  type="text"
+                  placeholder="example@mail.com"
+                />
+              </div>
+              <div className="input-password-container">
+                <label className="input-password-title">PASSWORD</label>
+                <input
+                  className="input-password"
+                  type="password"
+                  placeholder="******"
+                  autoComplete=""
+                />
+              </div>
+              <div className="button-login-container">
+                <button type="submit" className="login-button">
+                  Login
+                </button>
+              </div>
+
+              <h2 className="register-link">
+                Don’t have account? Register here
+              </h2>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
