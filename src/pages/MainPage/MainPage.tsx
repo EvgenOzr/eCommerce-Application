2d9971490feb6a738e892f342abd6dded8005071
EvgenOzr <131382405+EvgenOzr@users.noreply.github.com>
2025-05-06 23:42:13 +0500

    fix directory name

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Banner from '../../components/bunner/Banner';
+import Banner from '../../components/banner/Banner';
 import './MainPage.scss';
 
 const MainPage = () => {
