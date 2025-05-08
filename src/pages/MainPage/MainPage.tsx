6f9c20cbcab873012dd13467c3569204a4f9aa73
Kirill Medvedev <repkomail@gmail.com>
2025-05-08 20:08:59 +0200

    feat: add regPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import Banner from '../../components/banner/Banner';
-import './MainPage.scss';
+import React from "react";
+import Banner from "../../components/banner/Banner";
+import "./MainPage.scss";
 
 const MainPage = () => {
-    return (
-        <div className='main-container'>
-            <Banner textMain={'Discount 20% For New Member, '} textAdd={'ONLY FOR TODAY!!'}/>
-        </div>
-    );
+  return (
+    <div className="main-container">
+      <Banner
+        textMain={"Discount 20% For New Member, "}
+        textAdd={"ONLY FOR TODAY!!"}
+      />
+    </div>
+  );
 };
 
 export default MainPage;
--- src/types/css.d.ts
+++ src/types/css.d.ts
@@ -1,8 +1,8 @@
-declare module '*.scss' {
-    const css: { [key: string]: string };
-    export default css;
+declare module "*.scss" {
+  const css: { [key: string]: string };
+  export default css;
 }
-declare module '*.sass' {
-    const css: { [key: string]: string };
-    export default css;
+declare module "*.sass" {
+  const css: { [key: string]: string };
+  export default css;
 }
