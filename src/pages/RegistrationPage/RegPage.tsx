ab70b05ae7aa1ff9292572dd862efab4b5f369d9
EvgenOzr <131382405+EvgenOzr@users.noreply.github.com>
2025-05-19 18:59:02 +0500

    feat: add banner and registration set context

--- src/pages/RegistrationPage/RegPage.tsx
+++ src/pages/RegistrationPage/RegPage.tsx
@@ -4,9 +4,10 @@ import "./RegPage.scss";
 import { Link, useNavigate } from "react-router";
 import { RegistrationFormData } from "../../types/shopTypes";
 
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { registrationRequestResponse } from "../../API/RegisterUse";
 import { authError } from "../../API/ErrorApi";
+import { ShopContext } from "../../context/shopContext";
 
 export default function RegPage() {
   const {
@@ -28,6 +29,7 @@ export default function RegPage() {
   const [defaultAdress, setDefaultAdress] = useState(false);
   const [defaultBilling, setDefaultBilling] = useState(false);
   const [defaultShipping, setDefaultShipping] = useState(false);
+  const { setLogin, setIsLoginned } = useContext(ShopContext);
 
   const billingAdresses = useWatch({
     control,
@@ -47,7 +49,13 @@ export default function RegPage() {
 
   const formSubmit: SubmitHandler<RegistrationFormData> = async (data) => {
     try {
-      await registrationRequestResponse(data, defaultBilling, defaultShipping);
+      const response = await registrationRequestResponse(
+        data,
+        defaultBilling,
+        defaultShipping
+      );
+      setLogin(response.body.customer.email);
+      setIsLoginned(true);
       localStorage.setItem("registrationSuccess", "true");
       navigate("/");
     } catch (error) {
