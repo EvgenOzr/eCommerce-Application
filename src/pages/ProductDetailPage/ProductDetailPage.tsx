134dd029c592c80f27e2823daadb1880d48bbb0b
Kirill Medvedev <repkomail@gmail.com>
2025-05-22 23:36:15 +0200

    feat: implement detail page

--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { getProductsId } from "../../API/GetProductsId";
+import { ProductProjection } from "@commercetools/platform-sdk";
+import "./ProductDetailPage.scss";
+import { ClockLoader } from "react-spinners";
 
 export function ProductDetailPage() {
+  const [detailProduct, setDetailProduct] = useState<ProductProjection>();
   const { id } = useParams<{ id: string }>();
 
   useEffect(() => {
@@ -11,7 +15,7 @@ export function ProductDetailPage() {
     const fetchDetailProduct = async () => {
       try {
         const data = await getProductsId(id);
-        console.log(data);
+        setDetailProduct(data.body);
       } catch (error) {
         console.log(error);
       }
@@ -20,5 +24,36 @@ export function ProductDetailPage() {
     fetchDetailProduct();
   }, [id]);
 
-  return <div>ProductDetailPage</div>;
+  return (
+    <section className="item-container">
+      {detailProduct ? (
+        <>
+          <div className="item-container_image">
+            <img
+              src={detailProduct?.masterVariant.images?.[0].url}
+              alt=""
+              className="item-container_image_file"
+            />
+          </div>
+          <div className="item-container_product">
+            <h2 className="item-container_product_title">
+              {detailProduct?.name["en-GB"]}
+            </h2>
+            <p className="item-container_product_price">
+              {(detailProduct?.masterVariant.prices?.[0].value.centAmount ??
+                0) / 100}{" "}
+              $
+            </p>
+            <p className="item-container_product_description">
+              {detailProduct?.description?.["en-GB"]}
+            </p>
+          </div>
+        </>
+      ) : (
+        <div className="item-container_loader">
+          <ClockLoader size={150} color="#8b4513" />
+        </div>
+      )}
+    </section>
+  );
 }
