622a493ca6bcf8f9c3008fc939326040a559efe5
Kirill Medvedev <repkomail@gmail.com>
2025-05-22 19:40:28 +0200

    fix: change product count on page

--- src/API/GetProducts.tsx
+++ src/API/GetProducts.tsx
@@ -1,5 +1,12 @@
 import { apiRoot } from "./Client";
 
 export const getProducts = () => {
-  return apiRoot.productProjections().get().execute();
+  return apiRoot
+    .productProjections()
+    .get({
+      queryArgs: {
+        limit: 21,
+      },
+    })
+    .execute();
 };
