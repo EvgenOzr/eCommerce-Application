0d37d929c6df9a7ccf590e054df794050ee02b79
Kirill Medvedev <repkomail@gmail.com>
2025-05-27 18:51:07 +0200

    fix: fix path to modal image

--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -5,7 +5,7 @@ import { ProductProjection } from "@commercetools/platform-sdk";
 import "./ProductDetailPage.scss";
 import { ClockLoader } from "react-spinners";
 import saleIcon from "../../assets/images/Product/sale-icon.png";
-import ModalImage from "../../components/modalImage/modalImage";
+import ModalImage from "../../components/ModalImage/ModalImage";
 
 export function ProductDetailPage() {
   const [detailProduct, setDetailProduct] = useState<ProductProjection>();
