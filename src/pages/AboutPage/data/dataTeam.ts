f6f3264471e2ae4eeee15cd7a73a072c666a1734
EvgenOzr <131382405+EvgenOzr@users.noreply.github.com>
2025-06-12 11:56:25 +0500

    fix: add foto

--- src/pages/AboutPage/data/dataTeam.ts
+++ src/pages/AboutPage/data/dataTeam.ts
@@ -1,4 +1,5 @@
 import prog1 from "../../../assets/images/AboutPage/prog_1.jpg";
+import prog2 from "../../../assets/images/AboutPage/prog_2.jpg";
 import prog3 from "../../../assets/images/AboutPage/prog_3.jpg";
 
 const dataTeam = [
@@ -13,7 +14,7 @@ const dataTeam = [
     name: "Victoria",
     role: "Backend & frontend developer",
     info: "Live in Minsk. I worked as a security system design engineer for 5 years. Then for 5 years I worked as a site administrator. Now, I want to change direction and further develop myself. I am interested in areas where I could work with programming languages, mathematics. Hobbies: photography, drawing",
-    foto: "",
+    foto: prog2,
     github: "https://github.com/toryzb",
   },
   {
